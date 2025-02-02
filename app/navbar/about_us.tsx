49b8968d6511e43689589e46e0369524baa1eb74
Ganesh311047 <bollaganesh31@gmail.com>
2025-02-02 16:41:25 +0530

    Update about_us.tsx

--- app/navbar/about_us.tsx
+++ app/navbar/about_us.tsx
@@ -8,14 +8,14 @@ const AboutUs: React.FC = () => {
                 Welcome to TrenchStyle, your number one source for all things fashion. We're dedicated to giving you the very best of clothing, with a focus on quality, customer service, and uniqueness.
             </p>
             <p>
-                Founded in 2024 by Ganesh, TrenchStyle has come a long way from its beginnings in Hanamkonda. When Ganesh first started out, their passion for fashion drove them to start their own business.
+                Founded in 2024, TrenchStyle has come a long way from its beginnings in Hanamkonda. When Ganesh first started out, their passion for fashion drove them to start their own business.
             </p>
             <p>
                 We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.
             </p>
             <p>
                 Sincerely,<br />
-                Ganesh and team
+                team
             </p>
         </div>
     );
